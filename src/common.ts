562d54aa751e42b44773a10daeefa5c072fcfa61
KodyJKing <kody.j.king@gmail.com>
2019-09-05 18:56:34 -0700

    more cleanup

--- src/common.ts
+++ src/common.ts
@@ -1,27 +1,12 @@
 import * as fs from "fs";
 import * as path from "path";
 
-export function log( message: string ) {
-    console.log( `\x1b[36m${message}\x1b[0m` )
-}
-
 export function exists( file: string ) {
     try {
         return fs.statSync( file ) != null
     } catch ( e ) {
         return false
     }
-}
-
-export function readBuffer( file: string ): Buffer | null {
-    if ( !exists( file ) )
-        return null
-    return fs.readFileSync( file )
-}
-
-export function read( file: string ): string | null {
-    let buffer = readBuffer( file )
-    return buffer != null ? buffer.toString() : null
 }
 
 export function isFile( file: string ) {
@@ -34,7 +19,7 @@ export function isDirectory( file: string ) {
     return stats && stats.isDirectory()
 }
 
-function getFilesRecursiveInternal( root = __dirname, directory = root, files: string[] = [] ) {
+export function getFilesRecursive( root = __dirname, directory = root, files: string[] = [] ) {
     for ( let name of fs.readdirSync( directory ) ) {
         let fullname = path.join( directory, name )
         let stats = fs.statSync( fullname )
@@ -42,12 +27,8 @@ function getFilesRecursiveInternal( root = __dirname, directory = root, files: s
             let relative = path.relative( root, fullname )
             files.push( relative )
         } else if ( stats.isDirectory() ) {
-            getFilesRecursiveInternal( root, fullname, files )
+            getFilesRecursive( root, fullname, files )
         }
     }
     return files
-}
-
-export function getFilesRecursive( root = __dirname ) {
-    return getFilesRecursiveInternal( root )
 }
